6b73f7a9c8949e337ca325d825ecb1a97dad669d
vozaldi <vozaldi@gmail.com>
2026-06-16 13:06:47 +0700

    feat: Add global styles with Tailwind CSS

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import type { Route } from "./+types/root";
+import "../assets/styles/app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
